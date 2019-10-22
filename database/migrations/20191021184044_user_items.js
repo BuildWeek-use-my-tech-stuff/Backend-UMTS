9403f7a73730c274e8cb8a2415b89515b2970da4
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-22 13:32:06 -0400

    stuff

--- database/migrations/20191021184044_user_items.js
+++ database/migrations/20191021184044_user_items.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('user_items', tbl => {
       tbl.increments('id')
 
-      tbl.string('photo')
+      tbl.string('photo', 1000)
       tbl.string('item_name', 255).notNullable()
       tbl.text('description', 5000).notNullable()
       tbl.decimal('price', 14, 2)
