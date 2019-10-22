986d8c1d15a8e1edd40547af0273676256ef8122
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-21 21:37:24 -0400

    stuff

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/:id', user_itemsRouter)
+server.use('/api/:id/user_items', user_itemsRouter)
 
 server.get('/', (req, res) => {
     res.send("It's alive!");
