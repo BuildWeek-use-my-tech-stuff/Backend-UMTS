4e8020a6ba6a4318adbc7d167cf6ccc004474dda
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-23 13:34:08 -0400

    updated remove function

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -49,18 +49,31 @@ router.post('/', restricted, (req, res) => {
     });
   });
 
-  router.put('/:id', restricted, (req, res) => {
-    const id = req.params.id;
-    const changes = req.body;
-    
-    Items.editItem(changes, id)
-    .then(item => {
-      res.status(200).json(item)
-    })
-    .catch (err => {
-      res.status(500).json({ message: 'Failed to update item' });
-    });
-  });
+  // PUT for items
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  Items.editItem(id, changes)
+  .then(item => {
+      if(item){
+          res.status(200).json(item)
+      } else if(!id) {
+          res.status(404).json({
+              message: "The item with the specified ID does not exist."
+          })
+      } else if(!req.body.item_name || !req.body.description){
+          res.status(400).json({
+              errorMessage: "Please provide a item name and description"
+          })
+      }
+  })
+  .catch(error => {
+      console.log(error);
+      res.status(500).json({
+          errorMessage: "The item information can not be updated"
+      })
+  })
+})
 
   router.delete('/:id', (req, res) => {
     const { id } = req.params;
